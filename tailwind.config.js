ead325b6cf5d9976fb989082d8893a3eb360502f
Nikolai Petrov <nikolayuy@gmail.com>
2024-01-24 18:28:01 +0200

    images fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = withMT({
 	theme: {
 		extend: {
 			backgroundImage: {
-				"industrial-hall": "url('./src/assets/images/industrial-hall.jpg')",
-				"sveta-sofia": "url('./src/assets/images/sveta-sofia.png')",
-				provet: "url('./src/assets/images/provet.png')",
-				genika: "url('./src/assets/images/genika.png')",
-				pulmed: "url('./src/assets/images/pulmed.png')",
-				checkmark: "url('./src/assets/images/checkmark.png')",
-				contacts: "url('./src/assets/images/contacts.png')",
-				"product-image": "url('./src/assets/images/product-image.png')",
+				"industrial-hall": "url('./public/images/industrial-hall.jpg')",
+				"sveta-sofia": "url('./public/images/sveta-sofia.png')",
+				provet: "url('./public/images/provet.png')",
+				genika: "url('./public/images/genika.png')",
+				pulmed: "url('./public/images/pulmed.png')",
+				checkmark: "url('./public/images/checkmark.png')",
+				contacts: "url('./public/images/contacts.png')",
+				"product-image": "url('./public/images/product-image.png')",
 			},
 		},
 	},
